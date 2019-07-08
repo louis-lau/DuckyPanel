565f0af3a95645d4938de556d2ecdde7f9ccb044
Louis Laureys <louis@laureys.me>
2019-07-08 23:46:58 +0200

    beautify

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       files: ["*.js"],
       rules: {
-        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/no-var-requires": "off"
       }
     }
   ]
